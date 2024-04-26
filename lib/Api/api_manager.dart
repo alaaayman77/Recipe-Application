e73121c5603576421118a4ab202106c341aff184
MervatMagdy <mervetmagdy753@gmail.com>
2024-04-26 18:10:09 +0300

    search_update

--- lib/Api/api_manager.dart
+++ lib/Api/api_manager.dart
@@ -11,14 +11,14 @@ class ApiManager{
   static Future<RandomRecipeResponse?> getRandomRecipe({tags='' }) async {
     Uri url = Uri.https(ApiConstants.baseUrl, ApiConstants.randomRecipeApi,
         {
-          // "apiKey":"73f34b466856436e978b5cc90412f0d0",
+          "apiKey":"73f34b466856436e978b5cc90412f0d0",
           //"apiKey":"d6bc8f232b984351a3dac4324ea2227d",
        //   "apiKey":"1abadcbcdec042f2a1dc22b1c4499ce5",
         //  "apiKey":"1abadcbcdec042f2a1dc22b1c4499ce5",
        //  "apiKey":"d35ade602f354e3f8579b17700a20155",
          // "apiKey":"a9fe397ef4cc4d28a722da6d469f8c71",
-          // "apiKey": "73f34b466856436e978b5cc90412f0d0",
-       "apiKey":"73f34b466856436e978b5cc90412f0d0",
+         //  "apiKey": "73f34b466856436e978b5cc90412f0d0",
+       // "apiKey":"73f34b466856436e978b5cc90412f0d0",
        //   "apiKey":"9d23519846b7487ebb193fcb64c83c74",
           "number":"100",
           "tags":tags ,
