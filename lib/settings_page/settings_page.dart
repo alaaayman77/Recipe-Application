1c0a8448f8e5714fdee324e71952f2c6290e63a5
DELL <alaaaymanfouad@gmail.com>
2024-04-26 22:45:54 +0300

    settings

--- lib/settings_page/settings_page.dart
+++ lib/settings_page/settings_page.dart
@@ -1,7 +1,9 @@
+import 'package:final_project/error_model/error_item.dart';
 import 'package:final_project/settings_page/settings_container.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../Register/sign_up/sign_up.dart';
 import '../theming.dart';
 import 'info_screen.dart';
 
@@ -26,6 +28,34 @@ class _SettingsPageState extends State<SettingsPage> {
   void ContactUsBottomSheet(){
     showModalBottomSheet(context: context, builder: (BuildContext context)=> InfoScreen(content: '7201 W. 129th St., Ste 160Overland Park. KS 66213 Phone: 913.492.5900  •  800.227.7282 Fax: 913.492.5947Business Hours:Mon.-Fri. 7:45 am – 4:30 pm CST'));
   }
+  void ThemingBottomSheet(){
+    showModalBottomSheet(context: context, builder: (BuildContext context)=>Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Container(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            InkWell(child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Light Mode' , style: Theme.of(context).textTheme.titleMedium!.copyWith(color: Theming.primary),),
+                Icon(Icons.check_rounded , color: Theming.primary,)
+              ],
+            )),
+            InkWell(child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Dark Mode' , style: Theme.of(context).textTheme.titleMedium,),
+                Icon(Icons.check_rounded)
+              ],
+            )),
+
+          ],
+        ),
+      ),
+    ));
+  }
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -33,58 +63,85 @@ class _SettingsPageState extends State<SettingsPage> {
         Padding(
           padding: const EdgeInsets.all(16.0),
           child: Center(
-              child: SafeArea(
-                  child: Text(
-            'Settings',
-            style: Theme.of(context).textTheme.titleLarge,
-          ))),
+            child: SafeArea(
+              child: Text(
+                'Settings',
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+            ),
+          ),
         ),
-       Expanded(
-         child: Column(
+        Expanded(
+          child: Column(
+            children: [
+              SettingsContainer(title: 'About', openBottomSheet: AboutBottomSheet),
+              SizedBox(height: 20),
+              SettingsContainer(title: 'Contact us', openBottomSheet: ContactUsBottomSheet),
+              SizedBox(height: 20),
+              SettingsContainer(title: 'Theming', openBottomSheet: ThemingBottomSheet),
+              Spacer(),
+              InkWell(
+                onTap: () {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: Text('Logout Confirmation'),
+                        content: Text('Are you sure you want to logout?'),
+                        actions: <Widget>[
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: Text('No'),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pushNamed(SignUp.routeName);
 
-
-           children:
-         [
-           SettingsContainer(title: 'About', openBottomSheet:AboutBottomSheet ),
-           SizedBox(height: 20,),
-           SettingsContainer(title: 'Contact us', openBottomSheet:ContactUsBottomSheet ),
-           SizedBox(height: 20,),
-           SettingsContainer(title: 'Theming', openBottomSheet:ContactUsBottomSheet ),
-           Spacer(),
-           Padding(
-             padding: const EdgeInsets.all(32),
-             child: Container(
-               width: 200,
-               height: 110,
-               decoration: BoxDecoration(
-                 color: Theming.secondaryText.withOpacity(0.2),
-                 borderRadius: BorderRadius.circular(15),
-                 boxShadow: [
-                   BoxShadow(
-                     color: Colors.grey.withOpacity(0.5),
-                     spreadRadius: 5,
-                     blurRadius: 7,
-                     offset: Offset(0, 3), // changes position of shadow
-                   )
-                 ],
-               ),
-               child: Padding(padding: EdgeInsets.all(32.0), child :
-               Row(
-                 children: [
-                   Icon(Icons.logout_rounded, color: Colors.red,size: 30,),
-                   GestureDetector(onTap:(){
-
-
-                   } , child : Text('Logout' , style:Theme.of(context).textTheme.titleLarge!.copyWith(color: Colors.red),))
-                 ],
-               )),
-             ),
-           )
-
-          ],),
-       )
-
+                            },
+                            child: Text('Yes'),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(32.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Theming.primary.withOpacity(0.1),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: Offset(0, 3), // changes position of shadow
+                        )
+                      ],
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Logout',
+                            style: Theme.of(context).textTheme.bodyLarge!.copyWith(color: Colors.red),
+                          ),
+                          SizedBox(width: 10),
+                          Icon(Icons.logout_rounded, color: Colors.red, size: 30)
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        )
       ],
     );
-  }
-}
+  }}
